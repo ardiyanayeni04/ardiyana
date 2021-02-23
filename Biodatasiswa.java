06eb2773410895904c297c5445f5826ccd4d5fb0
ardiyanayeni04 <78910485+ardiyanayeni04@users.noreply.github.com>
2021-02-23 10:21:12 +0700

    Add files via upload

---
+++ Biodatasiswa.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodatasiswa;
+
+/**
+ *
+ * @author hp
+ */
+public class Biodatasiswa {
+    public static void main(String[] args) {
+	String nama, TTl, hobby;
+	int NIS;
+	nama = "Ardiyana yeni";
+	TTl = "malang,1 januari 2004";
+	hobby = "membaca,menyanyi";
+	NIS = (int) (9994/040.065);
+
+	System.out.println("nama lengkap = "+nama) ;
+	System.out.println("nomor registrasi ="+NIS) ;
+	System.out.println("tempat tanggal lahir = "+TTl) ;
+	System.out.println("hobby ="+hobby) ;
+        // TODO code application logic here
+    }
+}
